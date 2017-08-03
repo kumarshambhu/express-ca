a175291c3ee2dad7e74422a52374f0f0b0169533
Shambhu Kumar <shambhu.kumar@ge.com>
2017-08-03 14:53:51 +0530

    first Commit

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/index1', function(req, res, next) {
+    res.render('index1', { title: 'Express' });
+});
+router.get('/index2', function(req, res, next) {
+    res.render('index2', { title: 'Express' });
+});
+
+module.exports = router;
