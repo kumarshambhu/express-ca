a2ff25817c7813ceda99823dfa73a0d9a9764824
shambhu Kumar <shambhu.kumar06@GMAIL.COM>
2017-08-18 19:53:46 +0530

    adding contact

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/contact1', function(req, res, next) {
+    res.render('catalog/contact1', {title: 'Express'});
+});
+
+router.get('/contact2', function(req, res, next) {
+    res.render('catalog/contact2', {title: 'Express'});
+});
+
+router.get('/contact3', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+
 module.exports = router;
